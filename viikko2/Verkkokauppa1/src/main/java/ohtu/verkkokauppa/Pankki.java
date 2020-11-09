2d38b861f422480b734624d31a696dd6beb26698
Joel Kuronen <jkuronen95@gmail.com>
2020-11-09 21:43:34 +0200

    done viikko2

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -0,0 +1,24 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Pankki implements PankkiInterface {
+
+    private KirjanpitoInterface kirjanpito;
+
+    @Autowired
+    public Pankki(KirjanpitoInterface kirjanpito) {
+        this.kirjanpito = kirjanpito;
+    }
+
+    @Override
+    public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
+        kirjanpito.lisaaTapahtuma("tilisiirto: tililt?? " + tilille + " tilille " + tilille
+                + " viite " + viitenumero + " summa " + summa + "e");
+
+        // t????ll?? olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return true;
+    }
+}
